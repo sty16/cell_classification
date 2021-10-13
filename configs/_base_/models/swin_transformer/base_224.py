a1bd3b00dad9f96b55eac6e070b7fade96879e6f
sty <abeltianyu@gmail.com>
2021-10-13 20:43:12 +0800

    change

--- configs/_base_/models/swin_transformer/base_224.py
+++ configs/_base_/models/swin_transformer/base_224.py
@@ -23,6 +23,6 @@ model = dict(
         dict(type='Constant', layer='LayerNorm', val=1., bias=0.)
     ],
     train_cfg=dict(augments=[
-        dict(type='BatchMixup', alpha=0.8, num_classes=1000, prob=0.5),
-        dict(type='BatchCutMix', alpha=1.0, num_classes=1000, prob=0.5)
+        dict(type='BatchMixup', alpha=0.8, num_classes=11, prob=0.5),
+        dict(type='BatchCutMix', alpha=1.0, num_classes=11, prob=0.5)
     ]))
