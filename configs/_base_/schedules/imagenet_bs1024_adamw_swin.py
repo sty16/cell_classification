5a35a3dce5b4cc37234dd921b4174c5fac16e204
sty <abeltianyu@gmail.com>
2021-10-13 22:05:18 +0800

    change

--- configs/_base_/schedules/imagenet_bs1024_adamw_swin.py
+++ configs/_base_/schedules/imagenet_bs1024_adamw_swin.py
@@ -8,23 +8,28 @@ paramwise_cfg = dict(
 
 # for batch in each gpu is 128, 8 gpu
 # lr = 5e-4 * 128 * 8 / 512 = 0.001
-optimizer = dict(
-    type='AdamW',
-    lr=5e-4 * 128 * 8 / 512,
-    weight_decay=0.05,
-    eps=1e-8,
-    betas=(0.9, 0.999),
-    paramwise_cfg=paramwise_cfg)
-optimizer_config = dict(grad_clip=dict(max_norm=5.0))
+# optimizer = dict(
+#     type='AdamW',
+#     lr=5e-4 * 128 * 8 / 512,
+#     weight_decay=0.05,
+#     eps=1e-8,
+#     betas=(0.9, 0.999),
+#     paramwise_cfg=paramwise_cfg)
+# optimizer_config = dict(grad_clip=dict(max_norm=5.0))
 
 # learning policy
-lr_config = dict(
-    policy='CosineAnnealing',
-    by_epoch=False,
-    min_lr_ratio=1e-2,
-    warmup='linear',
-    warmup_ratio=1e-3,
-    warmup_iters=20 * 1252,
-    warmup_by_epoch=False)
+# lr_config = dict(
+#     policy='CosineAnnealing',
+#     by_epoch=False,
+#     min_lr_ratio=1e-2,
+#     warmup='linear',
+#     warmup_ratio=1e-3,
+#     warmup_iters=20 * 1252,
+#     warmup_by_epoch=False)
+
+## copy from resnet
+optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+lr_config = dict(policy='step', step=[30, 60, 90])
 
 runner = dict(type='EpochBasedRunner', max_epochs=300)
